24760499584eeca0628e5f4bbb573fe991db038a
Braedon McDonald <braedonmcdonald1@gmail.com>
2020-11-26 21:32:58 -0500

    Finish ugly layout

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import "./index.css";
+
 const App = () => {
     return (
-        <h1>HELLO</h1>
+        <div className="container">
+            <div className="row">
+                <textarea className="column" />
+                <div className="collumn column-left">
+                    <fieldset>
+                        <legend>Facebook Design</legend>
+                        <input type="radio" name="facebook_design" value="old" id="old"/>
+                        <label for="old">Old Facebook</label>
+                        <br />
+                        <input type="radio" name="facebook_design" value="new" id="new"/>
+                        <label for="new">New Facebook</label>
+                    </fieldset>
+                    <button type="button">Export to Word</button>
+                </div>
+            </div>
+        </div>
     )
 };
 
