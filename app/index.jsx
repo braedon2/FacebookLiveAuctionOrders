a5021e0786124c7cd63347173b56eb1acfb8fd3d
Braedon McDonald <braedonmcdonald1@gmail.com>
2020-11-26 21:40:58 -0500

    Make button disabled when no input

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import "./index.css";
 
 const App = () => {
+    const [textAreaValue, setTextAreaValue] = useState("");
+
     return (
         <div className="container">
             <div className="row">
-                <textarea className="column" />
+                <textarea 
+                    className="column" 
+                    onChange={(e) => setTextAreaValue(e.target.value)} 
+                    value={textAreaValue}
+                />
                 <div className="collumn column-left">
                     <fieldset>
                         <legend>Facebook Design</legend>
@@ -17,7 +23,7 @@ const App = () => {
                         <input type="radio" name="facebook_design" value="new" id="new"/>
                         <label for="new">New Facebook</label>
                     </fieldset>
-                    <button type="button">Export to Word</button>
+                    <button type="button" disabled={textAreaValue === ""}>Export to Word</button>
                 </div>
             </div>
         </div>
