180d68874471ce1bebbadfbdaa39a9842ffc43b5
Braedon McDonald <braedonmcdonald1@gmail.com>
2020-11-28 18:33:11 -0500

    Implement plaintext parsing for new facebook design

--- app/tests/parse.test.js
+++ app/tests/parse.test.js
@@ -1,7 +1,54 @@
 import { TestScheduler } from "jest";
 import { parseOrders, parseProfileLinks } from "../utils/parse";
 
-test('test the test', () => {
-    console.log("hello");
+const newDesign1 = `
+Top Fan
+Kaleigh Dieterle
+  · 48:56
+I'm tempted to order 2 more just to get over 5 and screw with you lol
+ · Reply · 6d
+
+Top Fan
+Richard Radillo
+  · 48:11
+Sold 1 dragon $57.50
+ · Reply · 6d
+Laura Frasher
+  · 48:08
+Sold 1 large dragon green 57.50
+ · Reply · 6d
+
+Top Fan
+Kaleigh Dieterle
+  · 47:58
+I ruined it for you lol
+ · Reply · 6d
+
+Top Fan
+Kaleigh Dieterle
+  · 47:38
+Sold 2 large dragons @ $57.50 - 1 blue/green, 1 pink/purple
+ · Reply · 6d
+Donna Funflower
+  · 47:15
+Wow great price
+ · Reply · 6d
+Donna Halden
+  · 46:39
+Forgot about the ear's lol
+ · Reply · 6d
+
+Top Fan
+Kaleigh Dieterle
+  · 46:49
+holy hell!
+`;
+
+
+
+test('new design', () => {
+    const orders = parseOrders(newDesign1, "new");
+    console.log(orders);
+    
     expect(true).toBe(true);
 });
--- app/utils/parse.js
+++ app/utils/parse.js
@@ -1,6 +1,21 @@
 
-export const parseOrders = (rawCommentString) => {
+export const parseOrders = (rawCommentString, designType) => {
+    const lines = rawCommentString.split("\n");
+    const regex = new RegExp("sold", "i");
+    const orders = {};
 
+    for (const [i, line] of lines.entries()) {
+        if (line.search(regex) !== -1) {
+            const name = lines[i - 2];
+            if (!orders[name]) {
+                orders[name] = [line];
+            }
+            else {
+                orders[name].push(line);
+            }
+        }
+    }
+    return orders;
 };
 
 export const parseProfileLinks = (rawCommentHtmlFragmentString) => {
