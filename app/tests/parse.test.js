290031728714bc45e9c5f27f7fce105458c058f8
Braedon McDonald <braedonmcdonald1@gmail.com>
2020-11-28 17:53:30 -0500

    Setup up jest

---
+++ app/tests/parse.test.js
@@ -0,0 +1,7 @@
+import { TestScheduler } from "jest";
+import { parseOrders, parseProfileLinks } from "../utils/parse";
+
+test('test the test', () => {
+    console.log("hello");
+    expect(true).toBe(true);
+});
